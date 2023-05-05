01c1bcbba76f84b4236fc27ae6f36ace90660867
Kirill Kuznetsov <67797289+senpachz@users.noreply.github.com>
2023-05-06 05:23:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import itertools
-# Считываем список слов из файла
-with open('wordlist.txt', 'r') as f:
-    words = f.read().splitlines()
+import os
 
-# Генерируем все возможные комбинации слов из списка
-combinations = list(itertools.product(words, repeat=12))
+# Получаем входные данные от пользователя
+words = []
+for i in range(8):
+    word = input(f'Введите {i+1}-ое слово: ')
+    words.append(word)
+
+# Считываем слова из файла словаря
+with open('dictionary.txt') as f:
+    dictionary_words = f.read().splitlines()
+
+# Генерируем все возможные уникальные комбинации слов из списка
+combinations = itertools.combinations(words + dictionary_words, 12)
+
+# Проверяем, что файл можно создать и записать
+filename = 'seed_phrases.txt'
+try:
+    with open(filename, 'w') as f:
+        pass
+    os.remove(filename)
+except IOError:
+    print(f'Ошибка: Не удалось создать файл {filename} или нет прав на запись в этот файл')
+    exit()
 
 # Сохраняем все комбинации в файл
-with open('seed_phrases.txt', 'w') as f:
-    for combination in combinations:
-        f.write(' '.join(combination) + '\n')
-
-print('Seed фразы успешно записаны в файл seed_phrases.txt')
+try:
+    with open(filename, 'w') as f:
+        for i, combination in enumerate(combinations):
+            if i >= 52428800:
+                break
+            f.write(' '.join(combination) + '\n')
+        print('Seed фразы успешно записаны в файл seed_phrases.txt')
+except IOError:
+    print('Ошибка: Не удалось записать seed фразы в файл')
